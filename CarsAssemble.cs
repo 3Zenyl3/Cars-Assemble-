d8661588b8f2909cda4d66011b54ce5b7d8a2cd6
3Zenyl3 <zenyl2007@gmail.com>
2025-10-21 22:27:49 +0500

    Create CarsAssemble.cs

---
+++ CarsAssemble.cs
@@ -0,0 +1,31 @@
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        if (speed == 0)
+            return 0;
+        else if (speed >= 1 && speed <= 4)
+            return 1;
+        else if (speed >= 1 && speed <= 4)
+            return 1;
+        else if (speed >= 5 && speed <= 8)
+            return 0.9;
+        else if (speed == 9)
+            return 0.8;
+        else
+            return 0.77;
+    }
+
+    public static double ProductionRatePerHour(int speed)
+    {
+        double a = SuccessRate(speed);
+        return a*221*speed;
+    }
+
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        double a = SuccessRate(speed);
+        int b = (int)(a * 221 * speed/60);
+        return b;
+    }
+}
